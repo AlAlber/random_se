0d06750005fe5220b22942414fad9c0df076c393
Alex Alber <alex.alber.aa@gmail.com>
2020-11-04 22:39:30 +0100

    End of Chapter 12

--- settings.py
+++ settings.py
@@ -14,6 +14,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 192, 203)
+        self.bullets_allowed = 3
 
         # Ship settings
         self.ship_speed = 1.5
